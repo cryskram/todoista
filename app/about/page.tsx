076d7904c05d5fa479e54366d11d50b76f182e22
cryskram <vageeshgn2005@gmail.com>
2025-10-17 18:58:25 +0530

    updated the website according to the session

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const thisiscool = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-red-400">This is a about us page</h1>
+      <h1 className="text-3xl text-green-700">This is a about us page</h1>
+      <h1 className="text-3xl text-green-700">This is a about us page</h1>
+      <h1 className="text-3xl text-green-700">This is a about us page</h1>
+      <h1 className="text-3xl text-green-700">This is a about us page</h1>
+    </div>
+  );
+};
+
+export default thisiscool;
